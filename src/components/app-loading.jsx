8462279ba7f6a73762db6d0563e0f71f30b1834e
Yakima-Teng <cleveryun@163.com>
2016-10-13 13:05:37 +0800

    added loaded effect

--- src/components/app-loading.jsx
+++ src/components/app-loading.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 
-export default class appHeader extends React.Component {
+export default class appLoading extends React.Component {
   constructor (props) {
     super(props)
-    this.state = {}
+    this.state = {
+      isLoading: true
+    }
+  }
+  componentDidMount () {
+    setTimeout(() => {
+      this.setState({
+        isLoading: false
+      })
+    }, 1000)
   }
   render () {
     return (
-      <div className="app-loading">app-loading</div>
+      <div className={'app-loading' + (this.state.isLoading ? '' : ' fade-out')}>
+        <div className="inner">
+          <i className="fa fa-spinner fa-pulse fa-5x"></i>
+        </div>
+      </div>
     )
   }
 }
