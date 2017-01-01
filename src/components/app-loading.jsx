5e35ef1e7cbdf43a1f2cd7b6699ea60744a95788
Yakima-Teng <cleveryun@163.com>
2017-01-01 22:47:41 +0800

    避免出现页面背景图在加载动画出现之前先出现的问题，预先加载页面所需其他图片元素

--- src/components/app-loading.jsx
+++ src/components/app-loading.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import $ from 'jquery'
 
 export default class appLoading extends React.Component {
   constructor (props) {
@@ -8,6 +9,19 @@ export default class appLoading extends React.Component {
     }
   }
   componentDidMount () {
+    // 避免出现页面背景图在加载动画出现之前先出现的问题
+    const img = new Image()
+    img.src = require('../assets/magic.jpg')
+    img.onload = () => {
+      $('body').css({
+        'background-image': `url("${img.src}")`
+      })
+    }
+
+    // 预先加载页面所需其他图片元素
+    for (var i = 1; i < 17; i++) {
+      new Image().src = `./static/img/near_1200x800/${i > 9 ? i : '0' + i}.jpg`
+    }
     setTimeout(() => {
       this.setState({
         isLoading: false
