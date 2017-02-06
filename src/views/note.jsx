f74717e3740196703bbdef5e90fbca111ab77301
Yakima-Teng <cleveryun@163.com>
2017-02-06 10:08:59 +0800

    update description of this SPA in note component

--- src/views/note.jsx
+++ src/views/note.jsx
@@ -8,12 +8,13 @@ export default class note extends React.Component {
       note: {
         title: 'Note',
         details: [
-          '这是一个简单的进阶教程：',
-          '1、“2D幻灯-JS”是一个很常规的JS代码；',
-          '2、“3D幻灯-CSS”在CSS3 animation的基础上，使用了CSS3 transform 3D变换；',
-          '3、“3D幻灯-JS”中改为由JS控制六面体的3D变换（根据鼠标位置相应地转动）；',
-          '4、该SPA所有图片均来自网络，版权归原作者所有。',
-          '5、为获得较好的视觉体验，请使用新版Chrome浏览器查看该SPA。'
+          '这是一些简单的小DEMOS：',
+          '1、“2D幻灯-JS”是一个很常规的JS幻灯片；',
+          '2、“3D幻灯-CSS”在CSS3 animation的基础上，使用CSS3 transform 3D变换实现的幻灯片；',
+          '3、“3D幻灯-JS”通过JS控制六面体的3D变换（根据鼠标位置相应地转动）；',
+          '4、“彩带”代码借鉴自VueJS作者的博客',
+          '5、该SPA所有图片均来自网络，版权归原作者所有。',
+          '6、为获得较好的视觉体验，请使用新版Chrome浏览器查看该SPA。'
         ]
       }
     }
